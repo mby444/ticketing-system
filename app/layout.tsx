077b390a59a05e5906d3cb1412cd1cd7944daeca
mby444 <bimayudha161@gmail.com>
2026-09-18 21:26:41 +0700

    Add auth actions

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { Toaster } from "sonner";
+import Navbar from "@/components/Navbar";
 // import Navbar from '@/components/Navbar';
 
 const geistSans = Geist({
@@ -30,7 +31,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         cz-shortcut-listen="true"
       >
-        {/* <Navbar /> */}
+        <Navbar />
         {children}
         <Toaster position="top-center" />
       </body>
